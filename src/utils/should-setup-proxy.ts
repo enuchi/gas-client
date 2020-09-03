9f9c5de00a5cf4492740ff141f533d8b4a17ba70
Guilherme Tod <guilhermetod@gmail.com>
2020-09-03 15:26:06 -0300

    Refactor: don't require process.env to setup proxy
    Suggestion/request: I am using this in an Angular App and the compiler
    doesn't pass the node variables to the file, which results in skipping
    the proxy setup, since process is not defined. Webpack does provide a
    way to mock or even actually pass the variables to the file, but I
    couldn't think of some situation where both the error messages and the
    production env would need to be checked. I think that just checking the
    message would suffice and allow for more flexible setups.

--- src/utils/should-setup-proxy.ts
+++ src/utils/should-setup-proxy.ts
@@ -1,3 +1,1 @@
-export default (err: Error): boolean =>
-  err.message === 'google is not defined' &&
-  process?.env.NODE_ENV === 'development';
+export default (err: Error): boolean => err.message === 'google is not defined';
