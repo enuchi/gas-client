7fe370ddfa8eab7811ec93e69207cb56075be48e
Guilherme Tod <guilhermetod@gmail.com>
2020-09-03 15:24:29 -0300

    Refactor: use error message instead of toString

--- src/utils/should-setup-proxy.ts
+++ src/utils/should-setup-proxy.ts
@@ -1,3 +1,3 @@
 export default (err: Error): boolean =>
-  err.toString() === 'ReferenceError: google is not defined' &&
+  err.message === 'google is not defined' &&
   process?.env.NODE_ENV === 'development';
